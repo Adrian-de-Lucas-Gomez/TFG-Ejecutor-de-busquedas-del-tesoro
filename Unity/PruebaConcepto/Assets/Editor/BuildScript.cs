319c470c5294585154eb0b668ef0c85bf27d26a0
Will7461 <43720307+Will7461@users.noreply.github.com>
2021-12-15 12:56:56 +0100

    autoBuild

---
+++ Unity/PruebaConcepto/Assets/Editor/BuildScript.cs
@@ -0,0 +1,37 @@
+using UnityEditor;
+using UnityEngine;
+using UnityEditor.Build.Reporting;
+
+// Output the build size or a failure depending on BuildPlayer.
+
+public class BuildScript : MonoBehaviour
+{
+	public static void PerformBuild()
+    {
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+
+		int sceneCount = UnityEngine.SceneManagement.SceneManager.sceneCountInBuildSettings;
+		string[] scenes = new string[sceneCount];
+		for (int i = 0; i < sceneCount; i++)
+		{
+			scenes[i] = UnityEngine.SceneManagement.SceneUtility.GetScenePathByBuildIndex(i);
+		}
+		buildPlayerOptions.scenes = scenes;
+        buildPlayerOptions.locationPathName = "Builds/AndroidBuild.apk";
+        buildPlayerOptions.target = BuildTarget.Android;
+        buildPlayerOptions.options = BuildOptions.None;
+
+        BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+        BuildSummary summary = report.summary;
+
+        if (summary.result == BuildResult.Succeeded)
+        {
+            Debug.Log("Build succeeded: " + summary.totalSize + " bytes");
+        }
+
+        if (summary.result == BuildResult.Failed)
+        {
+            Debug.Log("Build failed");
+        }
+    }
+}
---
+++ Unity/PruebaConcepto/autoBuild.bat
@@ -0,0 +1,2 @@
+echo Init Build
+S:\UnityHubEditors\2020.3.13f1\Editor\Unity.exe -quit -projectPath ./ -executeMethod BuildScript.PerformBuild
