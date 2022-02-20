53f53f1f69247acf962da083370db195b58dc839
Adrian-de-Lucas-Gomez <adridelu@ucm.es>
2022-02-20 19:31:59 +0100

    Modificacion del image charger provisional
    Es para poder meter los data al JSON por ahora. Luego se pondrá nombre internamente a las imagenes al poder subirlas.

--- React/src/components/steps/ImageCharger.tsx
+++ React/src/components/steps/ImageCharger.tsx
@@ -4,7 +4,17 @@ import axios from "axios"
 
 const ImageCharger = (props: StepComponentProps): JSX.Element => {
 
+
+    //Esto es como un using en c++
+    type FormElement = React.FormEvent<HTMLFormElement>;
+
     const [image, setImagen] = useState<File | undefined | null >(null);
+
+    //Donde almacenamos la pregunta
+    const [imageName, setName] = useState<string>("");
+
+    const [sobreEscribir, setSobreEscribir] = useState<boolean>(false);
+
     
     //OBTENIENDO LA IMAGEN
     const changeImagen = (e:React.ChangeEvent<HTMLInputElement>):void => {  
@@ -35,12 +45,67 @@ const ImageCharger = (props: StepComponentProps): JSX.Element => {
         }
     }
 
+    const modifyQuestion = (e:string):void =>{        
+        setName(e);
+    }
+
+    const guardaFase = (e:FormElement) => {
+        //Para que no se refresque la pagina en el onSubmit
+        e.preventDefault()
+        if (imageName !== ""){
+
+            //ME hago con el estado actual del array de info de la aventura
+            let new_state = props.getState<any>('DATA', []); 
+            //Preparo el diccionario que voy a meter en el registro
+            let myData = {tipo:"ImageStage" ,imagen: imageName};
+
+            console.log("Sobreescribir es igual a "+sobreEscribir);
+            if(sobreEscribir === true){
+                //De esta forma se puede meter el estado en unaposicion concreta en lugar de hacerlo en el final siempre
+                let position = props.getState<number>('FaseConfigurable',1);
+                new_state.splice(position,1,myData);
+            }
+            //Si no hay que sobreescribir nada simplemente pusheamos al final de los datos
+            else {
+                //Lo almaceno en la lista de fases que tengo disponibles
+                let position = props.getState<number>('WhereToPush',1);
+                new_state.splice(position, 0, myData);
+            }
+
+
+            //Y tras modificar la copia del registro para que me contenga pongo esta copia como el registro de la aventura
+            props.setState('DATA',new_state,[]);
+
+            //Importante aumentar el indice de donde estamos metiendo nuevos elementos a la aventura para que no 
+            //se metan todos en la posicion X y que luego estén TODOS EN ORDEN INVERSO
+            props.setState<number>('WhereToPush',props.getState<number>('WhereToPush',1)+1,1);
+        }
+        else{
+            alert("Rellena bien");
+        }
+    }
+
     return (
         <aside id="modal" className="modal">
             <div className="content-modal">
                 <input type="file" onChange={changeImagen} />
                 <button onClick={uploadImage}>GUARDAR IMAGEN</button>
             </div>
+            <br>
+            </br>
+
+            <h2 className="Titulo"  >Configuracion de evento de imagen:</h2>
+            <form onSubmit={e => e.preventDefault() }>
+                <h3>Añada aqui el nombre de la imagen (temporal, solo para probar)</h3>
+                <input className="form-control" type="text" required value={imageName} onChange ={ e =>modifyQuestion(e.target.value)}></input>
+            </form>
+            <br/>
+            <h2>Nombre actual de la imagen: {imageName}</h2>
+            <br/>
+            <br/>
+            <form onSubmit= {guardaFase}>
+                <button className='SaveButton' type="submit">Guardar Fase</button>
+            </form>
         </aside>
     )
 
