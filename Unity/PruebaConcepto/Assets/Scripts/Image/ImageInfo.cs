b485bc052e5e4c6361a206a9e75aece7a9c59cfa
Adrian-de-Lucas-Gomez <adridelu@ucm.es>
2022-02-20 14:18:47 +0100

    Carga de la imagen con escena propia
    Ya está metido en la jerarquia, solo falla que se ve el logo de la UCM por detrás por algún motivo.

---
+++ Unity/PruebaConcepto/Assets/Scripts/Image/ImageInfo.cs
@@ -0,0 +1,21 @@
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+
+public class ImageInfo : AdventureInfo
+{
+    public string nombreImagen;
+
+    /// <summary>
+    /// Metodo que recibe un objeto parseado de JSON y saca toda la informacion que necesite de este
+    /// </summary>
+    /// <param name="myInfo"> Objeto que salio de un JSON que contiene nuestra info </param>
+    public void readFromJSON(JObject myInfo)
+    {
+        //Me quedo con la escena a la que me tengo que ir para ejecutarme
+        stage = myInfo["tipo"].Value<string>();
+
+        //Obtengo los datos del quiz
+        nombreImagen = myInfo["imagen"].Value<string>();
+
+    }
+}
