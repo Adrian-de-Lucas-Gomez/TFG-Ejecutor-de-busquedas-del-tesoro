4089e5557d34175dec3e582d06a450208350e8fd
davidczepiel <dczepiel@ucm.es>
2022-02-27 16:19:17 +0100

    FIX: En linux se copiaba la carpeta entera de las imagenes y no solo su contenido

--- React/backend/GeneraZip.sh
+++ React/backend/GeneraZip.sh
@@ -9,7 +9,7 @@ cp -r -u -f ../../Unity/PruebaConcepto/Packages/ ../tmp/Packages/
 
 cp -r -u -f ../../Unity/PruebaConcepto/ProjectSettings/ ../tmp/ProjectSettings/
 
-cp -r -u -f ./Images/ ../tmp/Assets/Resources/AdventureImages/ 
+cp -r -u -f ./Images/. ../tmp/Assets/Resources/AdventureImages/ 
 
 zip -r ../Aventura.zip ../tmp
 
