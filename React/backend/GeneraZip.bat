f9a98e8a569d02133a97d7c77b79f590da370606
davidczepiel <dczepiel@ucm.es>
2022-03-06 22:02:32 +0100

    UPDATE: Ahora el json de la aventura se encuentra dentro del zip que te descargas
    Co-Authored-By: Adrian-de-Lucas-Gomez <47756657+Adrian-de-Lucas-Gomez@users.noreply.github.com>
    Co-Authored-By: Will7461 <43720307+Will7461@users.noreply.github.com>

--- React/backend/GeneraZip.bat
+++ React/backend/GeneraZip.bat
@@ -11,6 +11,9 @@ xcopy ..\..\Unity\PruebaConcepto\ProjectSettings\ ..\tmp\ProjectSettings\ /E /S 
 @REM Copiamos las imagenes que hayan en el servidor en la carpeta de resources para el futuro acceso del proyecto
 xcopy .\Images ..\tmp\Assets\Resources\AdventureImages /E /S /Q /Y
 
+@REM Muevo el json con los datos de la aventura a donde se encuentra el proyecto
+xcopy ..\AdventureData.json ..\tmp\Assets\AdventureData.json  /Y
+
 tar.exe -a -c -f ..\Aventura.zip ..\tmp
 
 rmdir ..\tmp /s /q
