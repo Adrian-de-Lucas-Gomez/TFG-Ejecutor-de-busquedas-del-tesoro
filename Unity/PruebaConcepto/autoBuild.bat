266f83b2cebcbe3d781305edde27654cbc274c2b
Will7461 <williamm@ucm.es>
2022-03-07 22:36:23 +0100

    Añadido -quit a autoBuild.bat

--- Unity/PruebaConcepto/autoBuild.bat
+++ Unity/PruebaConcepto/autoBuild.bat
@@ -1,2 +1,2 @@
 echo Init Build
-"C:/Program Files/Unity/Hub/Editor/2020.3.13f1/Editor/Unity.exe" -buildTarget Android -projectPath ./ -executeMethod BuildScript.PerformBuild
+"C:/Program Files/Unity/Hub/Editor/2020.3.13f1/Editor/Unity.exe" -quit -buildTarget Android -projectPath ./ -executeMethod BuildScript.PerformBuild
