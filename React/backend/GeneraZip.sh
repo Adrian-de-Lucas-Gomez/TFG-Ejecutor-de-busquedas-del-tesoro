2cde50b06d33593da9755a07c7961aaf27df77a9
davidczepiel <dczepiel@ucm.es>
2022-03-14 20:06:44 +0100

    ADD: añadido .sh bloqueante para ver mensajes por consola

--- React/backend/GeneraZip.sh
+++ React/backend/GeneraZip.sh
@@ -3,9 +3,10 @@ echo Generando Zip...
 
 #  Guardamos todos los elementos que forman la aventura en la "Base de datos" del server
 adventureName=$1
-mkdir ../BaseDeDatos/$adventureName
-cp -r -u -f ./Images/. ../BaseDeDatos/$adventureName/ 
-cp -u -f ../AdventureData.json ../BaseDeDatos/$adventureName/AdventureData.json  
+echo el nombre de la aventura es $adventureName y el del parametro es $1 
+mkdir -v ../BaseDeDatos/$adventureName
+cp -r -f ./Images/. ../BaseDeDatos/$adventureName/ 
+cp -v -f ../AdventureData.json ../BaseDeDatos/$adventureName/AdventureData.json  
 
 #  Se crea un directorio temporal en el que se van a agrupar todas las cosas necesarias para crear la aventura
 mkdir ../tmp
