c1164d36e0c56beda8bade5db785c1abb4bd819d
leoses <48771457+leoses@users.noreply.github.com>
2022-03-22 23:48:59 +0100

    ADD: funcionalidad de imageTaret básica

--- Unity/PruebaConcepto/Assets/Scripts/GameManager.cs
+++ Unity/PruebaConcepto/Assets/Scripts/GameManager.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Vuforia;
 
 public class GameManager : MonoBehaviour
 {
@@ -42,6 +43,12 @@ public class GameManager : MonoBehaviour
         if (_instance == null)
         {
             _instance = this;
+            /*
+             * Necesitamos cargar la aventura antes de que se cree cualquier escena puesto
+             * que la key de vuforia debe asignarse antes de que se cree cualquier escena que 
+             * use vuforia
+             */
+            loadAdventure();
             DontDestroyOnLoad(this);
         }
         //Si hay algo ya en esta posicion yo me destruyo
@@ -53,7 +60,6 @@ public class GameManager : MonoBehaviour
     {
         _preloadedScene = new KeyValuePair<string, AsyncOperation>("", null);
 
-        loadAdventure();
         loadAllSmallScenes();
     }
 
@@ -108,6 +114,8 @@ public class GameManager : MonoBehaviour
                     {
                         ImageTargetInfo newTarget = new ImageTargetInfo();
                         newTarget.readFromJSON((JObject)misFases[i]);
+                        //TO DO: reconfigurar la generación del JSON de react y sacar la key fuera de las fases
+                        VuforiaConfiguration.Instance.Vuforia.LicenseKey = newTarget.keyPackage;
                         adventureStages.Enqueue(newTarget);
                         break;
                     }
--- Unity/PruebaConcepto/Assets/Scripts/ImageTarget/ImageTargetStage.cs
+++ Unity/PruebaConcepto/Assets/Scripts/ImageTarget/ImageTargetStage.cs
@@ -1,75 +1,47 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using Vuforia;
 
 public class ImageTargetStage : Stage
 {
     ImageTargetInfo targetData;      //Datos referentes al target
-
+    private string pathToTarget;
     public override void Init(AdventureInfo data)
     {
         targetData = (ImageTargetInfo)data;
-        VuforiaConfiguration.Instance.Vuforia.LicenseKey = targetData.keyPackage;
-
-        //if (DataSet.Exists(targetData.nombrePackage))
-        //    Debug.Log("Existo!!");
-        //else
-        //    Debug.LogError("No esta instalado el database proporcionado");
-
-
-        //ObjectTracker objectTracker = TrackerManager.Instance.InitTracker<ObjectTracker>();
-        //objectTracker.Start();
-
-
-
-        //IEnumerable<DataSet> datasets = objectTracker.GetDataSets();
-        // Request an ImageTracker instance from the TrackerManager.
-        //ObjectTracker objectTracker = TrackerManager.Instance.GetTracker<ObjectTracker>();
-        //Debug.Log(datasets.ToList().Count);
-
-
-        // Create a new empty data set.
-        //DataSet dataSet = objectTracker.CreateDataSet();
-
-        //Debug.Log(targetData.nombrePackage);
-        //if (dataSet.Load(targetData.nombrePackage))
-        //{
-        //    Debug.Log("El dataset se ha cargado correctamente");
-        //    objectTracker.ActivateDataSet(dataSet);
-        //}
-        //else Debug.Log("Derrota");
-
         /*
-        var objectTracker = TrackerManager.Instance.GetTracker<ObjectTracker>();
+         * Las imagenes deben de estar almacenadas en la carpeta StreamingAssets/Vuforia, 
+         * TO DO: quitar el _scaled.jpg, ahora mismo es necesario porque la imagen que estamos usando
+         * por defecto es de un database de vuforia (que no está cargado) que le anade
+         * esa coletilla _scaled por defecto
+         */
+        pathToTarget = "Vuforia/" + targetData.nombreTarget + "_scaled.jpg";
+        VuforiaARController.Instance.RegisterVuforiaStartedCallback(CreateImageTargetFromSideloadedTexture);
+    }
 
-        // get the runtime image source and set the texture to load
-        var runtimeImageSource = objectTracker.RuntimeImageSource;
-        runtimeImageSource.SetFile(VuforiaUnity.StorageType.STORAGE_APPRESOURCE, "Vuforia/myTarget.jpg", 0.15f, "myTargetName");
+    private void CreateImageTargetFromSideloadedTexture()
+    {
+        ObjectTracker objectTracker = TrackerManager.Instance.GetTracker<ObjectTracker>();
 
-        // create a new dataset and use the source to create a new trackable
-        var dataset = objectTracker.CreateDataSet();
-        var trackableBehaviour = dataset.CreateTrackable(runtimeImageSource, targetData.nombreTarget);
+        // Coger en tiempo de ejecución la imagen y cargarla
+        RuntimeImageSource runtimeImageSource = objectTracker.RuntimeImageSource;
+        runtimeImageSource.SetFile(VuforiaUnity.StorageType.STORAGE_APPRESOURCE, pathToTarget, 0.15f, "CartelVuforia");
 
-        // add the DefaultTrackableEventHandler to the newly created game object
-        trackableBehaviour.gameObject.AddComponent<DefaultTrackableEventHandler>();
+        // Creamos un nuevo dataset y usamos la imagen para crear un objeto trackable
+        DataSet dataset = objectTracker.CreateDataSet();
+        DataSetTrackableBehaviour trackableBehaviour = dataset.CreateTrackable(runtimeImageSource, "ImageTarget");
 
-        // activate the dataset
+        // Añadimos al objeto que acabamos de crear un DefaultTrackableEventHandler
+        DefaultTrackableEventHandler behaviour = trackableBehaviour.gameObject.AddComponent<DefaultTrackableEventHandler>();
+        behaviour.StatusFilter = DefaultTrackableEventHandler.TrackingStatusFilter.Tracked;
+
+        // Activamos el dataset que acabamos de crear (y sobre el que hemos creado el nuevo target)
         objectTracker.ActivateDataSet(dataset);
 
-        // TODO: add virtual content as child object(s)
-        */
+        //TO ERASE: para comprobar que detecta el target que hemos creado
+        GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        cube.transform.SetParent(trackableBehaviour.transform);
+        cube.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
