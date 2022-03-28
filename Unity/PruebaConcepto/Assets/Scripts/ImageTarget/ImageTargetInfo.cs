b6907b2da06f1238a65f67152838468d9b2ec286
leoses <48771457+leoses@users.noreply.github.com>
2022-03-28 19:22:36 +0200

    ADD: añadida funcionalidad de mostrar texto en ImageTarget

--- Unity/PruebaConcepto/Assets/Scripts/ImageTarget/ImageTargetInfo.cs
+++ Unity/PruebaConcepto/Assets/Scripts/ImageTarget/ImageTargetInfo.cs
@@ -3,8 +3,13 @@ using UnityEngine;
 public class ImageTargetInfo : AdventureInfo
 {
     public string nombreTarget;
+    #region TO_ERASE
     public string nombrePackage;
     public string keyPackage;
+    #endregion
+    public bool hasText;
+    public bool showImage;
+    public string text = "";
 
     /// <summary>
     /// Metodo que recibe un objeto parseado de JSON y saca toda la informacion que necesite de este
@@ -16,12 +21,21 @@ public class ImageTargetInfo : AdventureInfo
         stage = myInfo["tipo"].Value<string>();
 
         //Obtengo los datos del imageTarget
+        #region T0_ERASE
         string aux = myInfo["Package"].Value<string>();
         string[] splitArray = aux.Split('.');
-
         nombrePackage = splitArray[0];
-        nombreTarget = myInfo["Target"].Value<string>();
         keyPackage = myInfo["Key"].Value<string>();
+        #endregion //Version antigua ImageTarget
+
+        nombreTarget = myInfo["Target"].Value<string>();
+        hasText = myInfo["AddText"].Value<bool>();
+        showImage = myInfo["ShowImage"].Value<bool>();
+
+        if (hasText)
+        {
+            text = myInfo["Text"].Value<string>();
+        }
 
     }
 }
--- Unity/PruebaConcepto/Assets/Scripts/ImageTarget/ImageTargetStage.cs
+++ Unity/PruebaConcepto/Assets/Scripts/ImageTarget/ImageTargetStage.cs
@@ -1,13 +1,44 @@
-using UnityEngine;
+using TMPro;
 using Vuforia;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
 
 public class ImageTargetStage : Stage
 {
     ImageTargetInfo targetData;      //Datos referentes al target
     private string pathToTarget;
+
+    DefaultTrackableEventHandler trackableHandler;
+    
+    [SerializeField] Button nextButton;
+    [SerializeField] GameObject pannel;
+    [SerializeField] TextMeshProUGUI textInfo;
+    [SerializeField] UnityEngine.UI.Image image;
+
+    private UnityEvent onFoundEvent;
     public override void Init(AdventureInfo data)
     {
+        onFoundEvent = new UnityEvent();
         targetData = (ImageTargetInfo)data;
+
+        //Boton para pasar a la siguiente fase
+        nextButton.gameObject.SetActive(false);
+        nextButton.onClick.AddListener(GameManager.getInstance().GoToNextPhase);
+
+        //Texto a mostrar cuando se encuentre el target
+        if (targetData.hasText)
+        {
+            textInfo.text = targetData.text;
+        }
+        pannel.SetActive(false);
+
+        if (targetData.showImage)
+        {
+            //TO DO: Habría que coger la textura de streamming assets y usarla 
+        }
+        image.gameObject.SetActive(false);
+
         /*
          * Las imagenes deben de estar almacenadas en la carpeta StreamingAssets/Vuforia, 
          * TO DO: quitar el _scaled.jpg, ahora mismo es necesario porque la imagen que estamos usando
@@ -31,17 +62,24 @@ public class ImageTargetStage : Stage
         DataSetTrackableBehaviour trackableBehaviour = dataset.CreateTrackable(runtimeImageSource, "ImageTarget");
 
         // Añadimos al objeto que acabamos de crear un DefaultTrackableEventHandler
-        DefaultTrackableEventHandler behaviour = trackableBehaviour.gameObject.AddComponent<DefaultTrackableEventHandler>();
-        behaviour.StatusFilter = DefaultTrackableEventHandler.TrackingStatusFilter.Tracked;
+        trackableHandler = trackableBehaviour.gameObject.AddComponent<DefaultTrackableEventHandler>();
+        trackableHandler.StatusFilter = DefaultTrackableEventHandler.TrackingStatusFilter.Tracked;
+
+        onFoundEvent.AddListener(OnTargetFoundAction);
+        trackableHandler.OnTargetFound = onFoundEvent;
+
+        Debug.Log(trackableHandler);
+        Debug.Log("Nombre del trackable handler: " + trackableHandler.gameObject.name);
 
         // Activamos el dataset que acabamos de crear (y sobre el que hemos creado el nuevo target)
         objectTracker.ActivateDataSet(dataset);
+    }
 
-        //TO ERASE: para comprobar que detecta el target que hemos creado
-        GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-        cube.transform.SetParent(trackableBehaviour.transform);
-        cube.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
-
+    private void OnTargetFoundAction()
+    {
+        if (targetData.hasText) pannel.SetActive(true);
+        if (targetData.showImage) image.gameObject.SetActive(true);
+        nextButton.gameObject.SetActive(true);
     }
 
 }
