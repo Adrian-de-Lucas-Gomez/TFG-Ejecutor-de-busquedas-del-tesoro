d0cc7c76cb211776c5c6f85c424af75ea17a45e7
leoses <48771457+leoses@users.noreply.github.com>
2022-04-09 17:40:56 +0200

    ADD: fase de GPS con react completa

--- React/frontend/src/components/steps/GPS.tsx
+++ React/frontend/src/components/steps/GPS.tsx
@@ -10,7 +10,7 @@ const GPS = (props: StepComponentProps): JSX.Element => {
     const [GPSLatitude, setGPSLatitude] = useState<number>(-3.733825);
     const [radius, setradius] = useState<number>(10.0);
     const [description, setdescription] = useState<string>("");
-
+    const [markers, setMarkers] = useState([40.453003, -3.733825]);
     type FormElement = React.FormEvent<HTMLFormElement>;
     const [sobreEscribir, setSobreEscribir] = useState<boolean>(false);
 
@@ -30,6 +30,7 @@ const GPS = (props: StepComponentProps): JSX.Element => {
             setGPSLatitude(estadoACargar.GPSLatitude);
             setradius(estadoACargar.radius);
             setdescription(estadoACargar.description);
+            setMarkers([estadoACargar.GPSLatitude,estadoACargar.GPSLongitude])
 
 
             //Nos aseguramos que lo que se esta configurando ahora es lo que nos hemos cargado
@@ -54,80 +55,55 @@ const GPS = (props: StepComponentProps): JSX.Element => {
         props.setState<any>('faseConfigurandose', myData, {});
     }
     const LocationMarker = () => {
-        //const [lat, setLat] = useState(null)
-        //const [long, setLong] = useState(null)
-        const [markers, setMarkers] = useState([
-            {
-                lat: 40,
-                lng: -95.6268544,
-            },
-        ]);
         const map = useMapEvents({
             click(e: any) {
-                setMarkers([...markers, e.latlng]);
-            }/*,
-            locationfound(e: any) {
-                setLong(e.latlng.lng);
-                setLat(e.latlng.lat)
-                map.flyTo(e.latlng, map.getZoom())
-            },*/
+                setMarkers(e.latlng);
+                prepareForSave(e.latlng.lng, e.latlng.lat, radius, description);
+                console.log("Market lng: " + (markers[1]) + " lat: " + (markers[0]));
+                console.log("Market REAL lng: " + (e.latlng.lng) + " lat: " + (e.latlng.lat));
+            }
         })
 
         return (
-            /*
-            <Marker position={[GPSLongitude, GPSLatitude]}>
-                <Popup>You are here</Popup>
-            </Marker>*/
             <>
-                {markers.map((marker, i) => (  // <---- parantheses good
-                    <Marker key={`marker-${i}`} position={marker}>
-                        <Popup>
-                            <span>
-                                A pretty CSS3 popup. <br /> Easily customizable.
-                            </span>
-                        </Popup>
-                    </Marker>
-                    ))}
-                
+                <Marker position={markers}>
+                    <Popup>
+                        <span>
+                            A pretty CSS3 popup. <br /> Easily customizable.
+                        </span>
+                    </Popup>
+                </Marker>
+
             </>
-        
-            
         )
     }
 
 
-return (
-    <div >
-        <h3 style={{ marginTop: '0.5%', marginBottom: '1%', fontSize: '200%' }} className="Titulo" >Configuración de fase geoposicionada</h3>
+    return (
+        <div >
+            <h3 style={{ marginTop: '0.5%', marginBottom: '1%', fontSize: '200%' }} className="Titulo" >Configuración de fase geoposicionada</h3>
 
-        <div>
-            <MapContainer center={[GPSLongitude, GPSLatitude]} zoom={13}>
-                <TileLayer
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                />
+            <div>
+                <MapContainer center={[GPSLongitude, GPSLatitude]} zoom={13}>
+                    <TileLayer
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    />
+                    <LocationMarker />
+                </MapContainer>
+            </div>
+            {/*Texto para el radio de acción*/}
+            <form className="center" style={{ marginBottom: '1%' }} onSubmit={e => e.preventDefault()}>
+                <input placeholder="Añada aqui el rango de deteccion de la localización" className='input-text' type="number" size={60} required value={radius} onChange={e => { prepareForSave(GPSLongitude, GPSLatitude, e.target.value as unknown as number, description); }}></input>
+            </form>
 
-                <Marker position={[GPSLongitude, GPSLatitude]}>
-                    <Popup>
-                        A pretty CSS3 popup. <br /> Easily customizable.
-                    </Popup>
-                </Marker>
+            {/*Texto para la descripcion de a donde debes llegar*/}
+            <form className="center" style={{ marginBottom: '1%' }} onSubmit={e => e.preventDefault()}>
+                <input placeholder="Texto para indicar al usuario que debe buscar" className='input-text' type="text" size={60} required value={description} onChange={e => { prepareForSave(GPSLongitude, GPSLatitude, radius, e.target.value); }}></input>
+            </form>
 
-                <LocationMarker />
-            </MapContainer>
+
+
         </div>
-        {/*Texto para el radio de acción*/}
-        <form className="center" style={{ marginBottom: '1%' }} onSubmit={e => e.preventDefault()}>
-            <input placeholder="Añada aqui el rango de deteccion de la localización" className='input-text' type="number" size={60} required value={radius} onChange={e => { prepareForSave(GPSLongitude, GPSLatitude, e.target.value as unknown as number, description); }}></input>
-        </form>
-
-        {/*Texto para la descripcion de a donde debes llegar*/}
-        <form className="center" style={{ marginBottom: '1%' }} onSubmit={e => e.preventDefault()}>
-            <input placeholder="Texto para indicar al usuario que debe buscar" className='input-text' type="text" size={60} required value={description} onChange={e => { prepareForSave(GPSLongitude, GPSLatitude, radius, e.target.value); }}></input>
-        </form>
-
-
-
-    </div>
-)
+    )
 };
 export default GPS;
