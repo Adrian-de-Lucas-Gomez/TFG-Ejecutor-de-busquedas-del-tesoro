883ee9e4bbaa6befc91e110beea720abc4b357b5
davidczepiel <dczepiel@ucm.es>
2022-04-11 20:44:22 +0200

    ADD: Añadida opción de descargar y guardar APKs hechas en el server

--- React/frontend/src/components/steps/AdventureCharger.tsx
+++ React/frontend/src/components/steps/AdventureCharger.tsx
@@ -5,13 +5,14 @@ import AdventureCard from "./AdventureCard"
 
 const AdventureCharger = (props: StepComponentProps): JSX.Element => {
 
-    //Texto que representa la aventura que estamos seleccionando para cargar
-    const [text, setText] = useState<string>("por defecto");
     //Indice de la aventtra que estamos seleccionando
     const [indiceAventura, setIndiceAventura] = useState<number>(0);
     //Lista de las aventuras que el server nos ha informado que existen y que podemos seleccionar y cargar
     const [aventurasDisponibles, setAventurasDisponibles] = useState<any>([]);
+    const [aplicacionesDisponibles, setAplicacionesDisponibles] = useState<any>([]);
 
+
+    const [aplicacionSubida, setAplicacionSubida]= useState<File | null >(null);
 
     //Nada más empezar lo que hago es pedirle al server las aventuras que tenemos disponibles para cargar 
     useEffect(() => {
@@ -20,16 +21,20 @@ const AdventureCharger = (props: StepComponentProps): JSX.Element => {
         console.log("Las opciones son "+response.data.Opciones);
         setAventurasDisponibles(response.data.Opciones);
         //Ponemos el texto a una cosa u otra para informar al jugador de lo que se está seleccionando
-        if(response.data.Opciones.length < 1)
-          setText("No hay aventuras disponibles");  
+        if(response.data.Opciones.length < 1){}
         else {
-          setText(response.data.Opciones[0]);
           setIndiceAventura(0);
-        }
-       })
-       .catch(function (error) {
-         //console.log(error);
-       });
+        }})
+
+        axios.get("./aplicacionesListas-guardadas").then (response => {
+          //Seteamos las aventuras que tenemos disponibles a lo que nos ha dicho el server
+          console.log("Las APKs son "+response.data.Opciones);
+          setAplicacionesDisponibles(response.data.Opciones);
+          //Ponemos el texto a una cosa u otra para informar al jugador de lo que se está seleccionando
+          if(response.data.Opciones.length < 1){}
+          else {
+            setIndiceAventura(0);
+          }})
 
 
         return () => {}
@@ -87,7 +92,6 @@ const AdventureCharger = (props: StepComponentProps): JSX.Element => {
     if(result <0) result = 0;    
     setIndiceAventura(result);
     console.log("La aventura a la que apuntamos ahora es "+aventurasDisponibles[result]);
-    setText(aventurasDisponibles[result]);
   } 
   const disminuirIndice = ():void =>{    
     var result = indiceAventura;
@@ -95,7 +99,6 @@ const AdventureCharger = (props: StepComponentProps): JSX.Element => {
     if(result < 0) result = 0;    
     setIndiceAventura(result);
     console.log("La aventura a la que apuntamos ahora es "+aventurasDisponibles[result]);
-    setText(aventurasDisponibles[result]);
   } 
 
   const ponerACargar = async (index:number)=>{
@@ -149,8 +152,60 @@ const AdventureCharger = (props: StepComponentProps): JSX.Element => {
     props.jump(0);
 }
 
+const descargarAventura = async (indice: number)=>{
+  let nombreAPK = aplicacionesDisponibles[indice];
+  console.log("vamos a solicitar algo llamado "+nombreAPK);
+  let aplicacion = await axios.get("./getAPK/"+nombreAPK, {responseType: 'arraybuffer',headers: {'Content-Type': 'application/json'},params: {json:"JSON.stringify(jsonFinal, null, 2)"}});
+  const type = aplicacion.headers['content-type']
+  const blob = new Blob([aplicacion.data], { type: type })
+  const link = document.createElement('a')
+  link.href = window.URL.createObjectURL(blob)
+  link.download = nombreAPK;
+  link.click();
+  link.remove();
+}
+
+
+
+const guardarAPKHecha = async (e:React.ChangeEvent<HTMLInputElement>) => {  
+  if (e.target.files?.item(0) instanceof File){
+    setAplicacionSubida( e.target.files?.item(0) as File)
+  }
+  else setAplicacionSubida(null);
+}
+
+const mandarAplicacion = async() =>{
+  //SI no tenemos una apk lista para subir no hacemos nada
+  if(!(aplicacionSubida instanceof File)) return;
+
+  const formData = new FormData();
+  formData.append("apk", aplicacionSubida as File, aplicacionSubida?.name);
+  //Hacemos una peticion POST a nuestro servidor a la route especificada
+  let result = await axios.post('/apk-upload', formData);
+}
+
   return (
     <div className = "App" >
+      <h1>Aplicaciones Hechas</h1>
+      <h4>Guardar aventura en el servidor</h4>
+      <input style={{fontSize:'150%'}} type="file" onChange={guardarAPKHecha} />
+      <button className="my-btn btn-outline-orange" style={{ fontSize: '170%' }} type="button" onClick={mandarAplicacion}>  Guardar  </button>
+
+      <br></br>
+      <br></br>
+
+
+        <div >
+            {
+          //@ts-ignore 
+          aplicacionesDisponibles.map((faseActual,ind) => (
+            <div>
+            <AdventureCard aventura = {faseActual} funcionCargar={descargarAventura} index={ind}></AdventureCard>
+            <br></br>
+            </div>
+          ))}
+        </div>
+
         <h1>Aventuras disponibles para cargar</h1>
         <div >
             {
